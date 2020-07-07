bef251e8a01f9a9cef43cafcb2beab450953ac3d
igorobed <43452966+igorobed@users.noreply.github.com>
2020-07-07 12:39:29 +0300

    Add files via upload

---
+++ TestLab1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Fraction c = new Fraction(3);
+                Fraction a = new Fraction(34, 27);
+                Fraction b = new Fraction(2, 4);
+                Console.WriteLine(b);
+                Console.WriteLine(a + b);
+                Console.WriteLine(b - c);
+                Console.WriteLine(c * b);
+                Console.WriteLine(a.ToString(3));
+                Console.WriteLine(a == a);
+                Console.WriteLine(c > b);
+                Console.WriteLine(c <= b);
+                Fraction g = new Fraction(-1, 0);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
