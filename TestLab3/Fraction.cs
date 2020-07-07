df403141d76c45ed033c3a9de2ae53f8f81c2157
igorobed <43452966+igorobed@users.noreply.github.com>
2020-07-07 12:41:03 +0300

    Add files via upload

---
+++ TestLab3/Fraction.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLab3
+{
+    class Fraction : ICloneable, IComparable, IEquatable<Fraction>, IComparable<Fraction>
+    {
+        //numerator - числитель
+        private BigInteger _numerator;
+        //denominator - знаменатель
+        private BigInteger _denominator;
+
+        #region constructors
+        public Fraction()
+        {
+            _numerator = 1;
+            _denominator = 1;
+        }
+
+        //подразумевается, что в знаменателе стоит 1
+        public Fraction(BigInteger num)
+        {
+            _numerator = num;
+            _denominator = 1;
+        }
+        public Fraction(BigInteger num, BigInteger denom)
+        {
+            if (denom == 0)
+            {
+                throw new ArgumentException("Denominator can not be zero");
+            }
+            _numerator = num;
+            _denominator = denom;
+            if (_denominator < 0)
+            {
+                _numerator = _numerator * (-1);
+                _denominator = _denominator * (-1);
+            }
+            BigInteger temp = NOD(_numerator, _denominator);
+            _numerator = _numerator / temp;
+            _denominator = _denominator / temp;
+        }
+        #endregion
+
+        #region properties
+        public BigInteger Numerator
+        {
+            get
+            {
+                return _numerator;
+            }
+        }
+
+        public BigInteger Denominator
+        {
+            get
+            {
+                return _denominator;
+            }
+        }
+        #endregion
+
+        #region ToString
+        public override string ToString()
+        {
+            return $"<{_numerator}>/<{_denominator}>";
+        }
+
+        //печать с заданным числом знаков после запятой
+        public string ToString(int n)
+        {
+            double result = (double)this._numerator / (double)this._denominator;
+            string form = "#.";
+            for (int i = 0; i < n; i++)
+            {
+                form += '#';
+            }
+            return result.ToString(form);
+        }
+        #endregion
+
+        #region MathAlgorithms
+        //поиск наибольшего общего делителя(Алгоритм Евклида)
+        private static BigInteger NOD(BigInteger a, BigInteger b)
+        {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("NOD incorrect arguments");
+            }
+            if (a < 0) a = -1 * a;
+            if (b < 0) b = -1 * b;
+            while (b != 0)
+            {
+                BigInteger temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        //поиск наименьшего общего кратного(нужно для сложения и вычитания)
+        private static BigInteger NOK(BigInteger a, BigInteger b)
+        {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("NOK incorrect arguments");
+            }
+            if (a < 0) a = -1 * a;
+            if (b < 0) b = -1 * b;
+            /*if ((a * b) < 0)
+            {
+                return (-1 * (a * b)) / NOD(a, b);
+            }
+            else
+            {
+                return (a * b) / NOD(a, b);
+            }*/
+            return (a * b) / NOD(a, b);
+        }
+
+        private static Fraction Abs(Fraction x)
+        {
+            if (x is null)
+            {
+                throw new NullReferenceException("An empty object is passed");
+            }
+            if (x._numerator < 0)
+            {
+                return new Fraction(-1 * x._numerator, x._denominator);
+            }
+            else
+            {
+                return new Fraction(x._numerator, x._denominator);
+            }
+        }
+
+        //метод Ньютона
+        public Fraction SqrtNewton(Fraction eps)
+        {
+            if (eps <= (new Fraction(0)))
+            {
+                throw new ArgumentException("eps must be greater than zero");
+            }
+            if (this._numerator < 0)
+            {
+                throw new ArgumentException("There must be a number greater than or equal to zero under the root");
+            }
+            Fraction x = new Fraction();
+            Fraction frConst = new Fraction(2);
+            for (; ; )
+            {
+                Fraction nx = (x + (this / x)) / frConst;
+                if (Abs(x - nx) < eps)
+                {
+                    break;
+                }
+                x = (Fraction)nx.Clone();
+            }
+            return x;
+        }
+        #endregion
+
+        #region OverloadingArithmeticOperators
+        public static Fraction Sum(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            BigInteger nok = NOK(first._denominator, second._denominator);
+            return new Fraction((nok / first._denominator) * first._numerator + (nok / second._denominator) * second._numerator, nok);
+        }
+
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            return Sum(first, second);
+        }
+
+        public static Fraction Minus(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            BigInteger nok = NOK(first._denominator, second._denominator);
+            return new Fraction((nok / first._denominator) * first._numerator - (nok / second._denominator) * second._numerator, nok);
+        }
+
+        public static Fraction operator -(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            return Minus(first, second);
+        }
+
+        public static Fraction Mul(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            return new Fraction(first._numerator * second._numerator, first._denominator * second._denominator);
+        }
+
+        public static Fraction operator *(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            return Mul(first, second);
+        }
+
+        public static Fraction Div(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            if (second._numerator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(first._numerator * second._denominator, first._denominator * second._numerator);
+        }
+
+        public static Fraction operator /(Fraction first, Fraction second)
+        {
+            if (first is null || second is null)
+            {
+                throw new ArgumentException("Argument is null");
+            }
+            if (second._numerator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return Div(first, second);
+        }
+
+        public Fraction Pow(int n)
+        {
+            if (n == 0)
+            {
+                return new Fraction(1, 1);
+            }
+            if (n > 6 || n < (-6))
+            {
+                throw new ArgumentException("Too much degree");
+            }
+
+            if (this._numerator == 0)
+            {
+                return new Fraction(0);
+            }
+            BigInteger numer;
+            BigInteger denom;
+            if (n > 0)
+            {
+                numer = this._numerator;
+                denom = this._denominator;
+
+            }
+            else
+            {
+                numer = this._denominator;
+                denom = this._numerator;
+            }
+            n = Math.Abs(n);
+            for (int i = 1; i < n; i++)
+            {
+                numer *= this._numerator;
+                denom *= this._denominator;
+            }
+
+            return new Fraction(numer, denom);
+
+        }
+        #endregion
+
+
+        #region interfaces
+        public object Clone()
+        {
+            return new Fraction(this._numerator, this._denominator);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is null)
+            {
+                return 1;
+            }
+
+            if (obj is Fraction)
+            {
+                return CompareTo(obj as Fraction);
+            }
+
+            throw new ArgumentException("Object is not a Fraction");
+        }
+
+        public int CompareTo(Fraction other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            BigInteger first = other._denominator * this._numerator;
+            BigInteger second = other._numerator * this._denominator;
+            return first.CompareTo(second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+            if (obj is Fraction)
+            {
+                return Equals(obj as Fraction);
+            }
+            return false;
+        }
+
+        public bool Equals(Fraction obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+            if (_numerator == obj._numerator && _denominator == obj._denominator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region OverloadingLogicalOperators
+        public static bool operator ==(Fraction first, Fraction second)
+        {
+            if (ReferenceEquals(first, null) && ReferenceEquals(second, null))//можн через is или ==
+            {
+                return true;
+            }
+            else if (first is object && second is null)
+            {
+                return first.Equals(second);
+            }
+            else if (first is null && second is object)
+            {
+                return second.Equals(first);
+            }
+            else
+            {
+                return first.Equals(second);
+            }
+        }
+
+        public static bool operator !=(Fraction first, Fraction second)
+        {
+            return !(first == second);
+        }
+
+        public static bool operator >(Fraction first, Fraction second)
+        {
+            return first.CompareTo(second) == 1;
+        }
+
+        public static bool operator <(Fraction first, Fraction second)
+        {
+            return first.CompareTo(second) == -1;
+        }
+
+        public static bool operator >=(Fraction first, Fraction second)
+        {
+            return first.CompareTo(second) >= 0;
+        }
+
+        public static bool operator <=(Fraction first, Fraction second)
+        {
+            return first.CompareTo(second) <= 0;
+        }
+        #endregion
+    }
+}
---
+++ TestLab3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLab3
+{
+    class Program
+    {
+        delegate Fraction MyFunction(Fraction x); 
+        static void Main(string[] args)
+        {
+            try
+            {
+                //задаю интервал
+                Fraction[] interval = new Fraction[2] {new Fraction(-6), new Fraction(-3)};
+                //задаю точность
+                Fraction EPS = new Fraction(1, 10000);
+                //присваиваю значение делегату
+                MyFunction @delegate = f1;
+                //вызываю функцию и печатую значение
+                Fraction r = Func(interval, @delegate, EPS);
+                Console.WriteLine(r.ToString(3));
+                Console.WriteLine(f1(r).ToString(7));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static Fraction f1(Fraction x)
+        {
+            return x * x - (new Fraction(10));
+        }
+
+        private static Fraction f2(Fraction x)
+        {
+            return x * x - (new Fraction(5)) * x + (new Fraction(4));
+        }
+
+        private static Fraction f3(Fraction x)
+        {
+            return (new Fraction(3)) * x - (new Fraction(5));
+        }
+
+        //поиск корня уравнения методом дихотоми    [a,b]
+        private static Fraction Func(Fraction[] interval, MyFunction del, Fraction eps)
+        {
+            Fraction fr_0 = new Fraction(0);
+            if ((del(interval[0]) * del(interval[1])) >= fr_0)
+            {
+                throw new ArgumentException("The specified interval does not meet the requirements of the algorithm");
+            }
+            
+            Fraction a = interval[0];
+            Fraction b = interval[1];
+
+            if (b <= a)
+            {
+                throw new ArgumentException("Incorrect interval: b <=");
+            }
+            Fraction c;
+            Fraction fr_2 = new Fraction(2);
+            
+            while ((b - a) > eps)
+            {
+                c = (a + b) / fr_2;
+                if ((del(b) * del(c)) < fr_0)
+                {
+                    a = (Fraction)c.Clone();
+                }
+                else
+                {
+                    b = (Fraction)c.Clone();
+                }
+            }
+            return (a + b) / fr_2;
+        }
+    }
+}
